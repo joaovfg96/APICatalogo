a76cd54d7dc02ada51bebbf84a835d4022175d7a
João Victor <joaovfg13@gmail.com>
2023-09-15 18:26:00 -0300

    Add project files.

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,108 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriasController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public CategoriasController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("produtos")]
+    public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+    {
+        return _context.Categorias.Include(p => p.Produtos).AsNoTracking().ToList();
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Categoria>> Get()
+    {
+        try
+        {
+            var categorias = _context.Categorias.AsNoTracking().ToList();
+            if (categorias is null)
+            {
+                return NotFound("Nenhuma categoria encontrada");
+            }
+
+            return categorias;
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Ocorreu um problema ao tratar a sua solicitação");
+        }
+        
+    }
+
+    [HttpGet("{id:int}", Name = "ObterCategoria")]
+    public ActionResult<Categoria> Get(int id)
+    {
+        try
+        {
+            var categoria = _context.Categorias.AsNoTracking().FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound("Categoria não encontrada");
+            }
+
+            return categoria;
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Ocorreu um problema ao tratar a sua solicitação");
+        }
+    }
+
+    [HttpPost]
+    public ActionResult Post(Categoria categoria)
+    {
+        if(categoria is null)
+        {
+            return BadRequest();
+        }
+
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Categoria categoria)
+    {
+        if(id != categoria.CategoriaId)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(categoria).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(categoria);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+        if(categoria is null)
+        {
+            return NotFound("Categoria não encontrada");
+        }
+
+        _context.Categorias.Remove(categoria);
+        _context.SaveChanges();
+
+        return Ok(categoria);
+    }
+}
